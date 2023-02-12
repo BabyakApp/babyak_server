5420171b037df0746639a18103be182ae567fbe1
Taeyoung Kim <lucy_ia@ewhain.net>
2023-02-12 10:19:43 +0900

    BAB-18 Post api

--- src/main/java/com/babyak/babyak/domain/favorite/FavoriteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.babyak.babyak.domain.favorite;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
-    boolean existsByUserIdAndPostId(Integer userId, Integer postId);
-    Favorite findByUserIdAndPostId(Integer userId, Integer postId);
-}
--- src/main/java/com/babyak/babyak/domain/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.babyak.babyak.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
